9cf1524fae7fad53ec5c92f1556e27e796e7ddd4
marcrleonard <marc.r.leonard@gmail.com>
2019-10-27 20:51:07 -0600

    Initial commit of vue app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <a href="/#/home"><img  alt="Vue logo" src="./assets/Logo_ML.png"></a>
+
+  <div class="hello">
+    <p>A mixed-bag of creations</p>
+  </div>
+  <nav>
+  <ul>
+
+  <li><router-link to="/">Home</router-link></li>
+  <li><router-link to="/work">Work</router-link></li>
+  <li><router-link to="/about">About</router-link></li>
+  </ul>
+  <router-view></router-view>
+  </nav>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import VueRouter from 'vue-router'
+
+//This About is an example of how to lazy load a route.
+const About = () => import( './views/About.vue')
+
+import Home from './views/Home.vue'
+import Work from './views/Work.vue'
+
+
+// const Foo = () => import('./Foo.vue')
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: Home },
+    { path: '/work', component: Work },
+    { path: '/about', component: About }
+  ]
+});
+
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'Container',
+  props: {
+    msg: String
+  },
+  router:router
 }
 </script>
 
-<style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +58,27 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+
+}
+.router-link-exact-active {
+  border-bottom: 3px solid black;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  text-decoration: none;
+  color: black;
+  font-size:20px;
+  font-weight: bold;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
-Vue.config.productionTip = false
+Vue.use(Vuex);
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
 
 new Vue({
+  store,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
